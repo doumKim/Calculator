04ed0b9f69ab64ac751917a04045407f5bcc4584
Doum Kim <eha0129@naver.com>
2020-04-16 14:03:48 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+def plus(a,b):
+  return int(a) + int(b)
+
+def minus(a,b):
+  return int(a) - int(b)
+
+def times(a,b):
+  return int(a) * int(b)
+
+def division(a,b):
+  return int(a) / int(b)
+
+def reminder(a,b):
+  return int(a) % int(b)
+
+def negation(a):
+  return -int(a)
+
+def power(a,b):
+  return int(a) ** int(b)
+
+print(plus(2,2))
+print(minus(2,2))
+print(times(2,2))
+print(division(2,2))
+print(reminder(2,2))
+print(negation(2))
+print(power(2,2))
